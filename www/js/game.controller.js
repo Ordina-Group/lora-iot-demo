80501e3e3f957fe891b55e02fb2a40c25b3b7e94
kevindahub <kevin.van.houtte1@gmail.com>
2015-10-29 11:29:50 +0100

    modified the jquery into the angular framework

--- www/js/game.controller.js
+++ www/js/game.controller.js
@@ -8,7 +8,50 @@
 
     function GameCtrl($scope, $location) {
 
-        console.log("gamectrl loaded");
+        $(document).ready(function () {
+            var machine1 = $("#machine1").slotMachine({
+                active: 1,
+                delay: 300
+            });
+
+            var machine2 = $("#machine2").slotMachine({
+                active: 1,
+                delay: 300
+            });
+
+            var machine3 = $("#machine3").slotMachine({
+                active: 1,
+                delay: 300
+            });
+
+            function onComplete(active) {
+                switch (this.element[0].id) {
+                    case 'machine1':
+                        $("#machine1Result").text("Index: " + this.active);
+                        break;
+                    case 'machine2':
+                        $("#machine2Result").text("Index: " + this.active);
+                        break;
+                    case 'machine3':
+                        $("#machine3Result").text("Index: " + this.active);
+                        break;
+                }
+            }
+
+            $("#slotMachineButton1").click(function () {
+
+                machine1.shuffle(5, onComplete);
+
+                setTimeout(function () {
+                    machine2.shuffle(5, onComplete);
+                }, 500);
+
+                setTimeout(function () {
+                    machine3.shuffle(5, onComplete);
+                }, 1000);
+
+            })
+        });
 
     }
 
