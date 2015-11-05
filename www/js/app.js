89cb9303d0d1aa7b8feabbf4a1a415ce48a27bed
kevindahub <kevin.van.houtte1@gmail.com>
2015-11-05 11:59:52 +0100

    Changed color theme to orange and put a reguliere expression for a valid email

--- www/js/app.js
+++ www/js/app.js
@@ -28,5 +28,11 @@
         .config(function (RestangularProvider) {
             RestangularProvider.setBaseUrl('http://localhost:8080/');
             RestangularProvider.setDefaultHeaders({'Content-Type': 'application/json'});
-        });
+        })
+        .config(function ($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('orange')
+                .accentPalette('amber')
+                .warnPalette('deep-orange');
+        })
 })();
