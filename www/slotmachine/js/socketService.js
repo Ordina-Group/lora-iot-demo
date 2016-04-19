e8ed69f216597e5029b5c8615c7f64185e361bea
yave <yannick.vergeylen@gmail.com>
2016-04-19 09:10:33 +0200

    dynamic websocket

--- www/slotmachine/js/socketService.js
+++ www/slotmachine/js/socketService.js
@@ -1,26 +1,30 @@
 (function () {
     'use strict';
 
-    angular .module('devoxx')
-            .service("nodeSocketService", function() {
+    angular.module('devoxx')
+        .service("nodeSocketService", nodeSocketService);
+
+    nodeSocketService.$inject = ['$window'];
+
+    function nodeSocketService($window) {
 
         //Internal variables.
-        var nodeSocket  = null;
-        var callbacks   = [];
+        var nodeSocket = null;
+        var callbacks = [];
 
         //Kick things into gear!
         connectToWebsocket();
 
-        this.sendJSONMessage = function(jsonMessage) {
+        this.sendJSONMessage = function (jsonMessage) {
             nodeSocket.send(JSON.stringify(jsonMessage));
         };
 
-        this.registerCallback = function(callback) {
+        this.registerCallback = function (callback) {
             callbacks.push(callback);
         };
 
         function connectToWebsocket() {
-            nodeSocket = new WebSocket("ws://sensorlabs-frontend-itt.cloudapp.net:7081");
+            nodeSocket = new WebSocket("ws://"+$window.location.hostname+":7081");
 
             //Wait for the socket connection to be established before doing anything else socket related!
             nodeSocket.onopen = function (event) {
@@ -30,12 +34,11 @@
                     console.log("Received message from web socket:" + event.data);
                     var data = JSON.parse(event.data);
 
-                    for(var i = 0; i < callbacks.length; i++) {
+                    for (var i = 0; i < callbacks.length; i++) {
                         callbacks[i](data);
                     }
                 }
             };
         }
-    });
-
+    }
 })();
