057f3447140ebfdfa4befa90a87b21d903cc342e
yave <yannick.vergeylen@gmail.com>
2016-04-19 14:59:49 +0200

    playing around with the giroscope if existing

--- www/booze/meter.controller.js
+++ www/booze/meter.controller.js
@@ -9,31 +9,59 @@
     function MeterCtrl($scope, socketService, $window) {
 
 
-
         var meter = this;
-        var startHeight = 585;
-
-        meter.orientation = {};
-
-        window.addEventListener("deviceorientation",function(val) {
-            console.log(val);
-            meter.orientation = val;
-        });
+        //meter.orientation = [0, 0, 0];
+        //
+        //window.addEventListener("deviceorientation", function (val) {
+        //    if (tresholdReached()) {
+        //        //console.log('x', val.alpha>0 ? val.alpha : 360+val.alpha);
+        //        //console.log('y', val.beta>0 ? val.beta : 360+val.beta);
+        //        console.log('z', val.gamma>0 ? 'left' : 'right');
+        //
+        //        //meter.orientation[0] = val.alpha;
+        //        //meter.orientation[1] = val.beta;
+        //        meter.orientation[2] = val.gamma;
+        //    }
+        //
+        //    function tresholdReached() {
+        //        //if (Math.abs(meter.orientation[0]-val.alpha) > 1) {
+        //        //    return true;
+        //        //}
+        //        //if (Math.abs(meter.orientation[1]-val.beta) > 1) {
+        //        //    return true;
+        //        //}
+        //        if (Math.abs(meter.orientation[2]-val.gamma) > 1) {
+        //            return true;
+        //        }
+        //        return false;
+        //    }
+        //});
 
 
         socketService.registerCallback(messageReceived);
 
         function messageReceived(data) {
-            $('#rect4384-5')[0].setAttribute('d',composePath((data * 5.58)));
+            $('#rect4384-5')[0].setAttribute('d', composePath((data * 5.58)));
         }
 
-        function composePath (data) {
-            return 'm 320,' + (620-data) + ' ' +
-            'c -9.07445,0.07891 -39.11956,1.703858 -39,9.499835 ' +
-            'l 0,' + data + ' ' +
-            'c -0.0299,7.72957 30.60495,9.51173 39,9.49984 8.87795,0.0946 39.09375,-1.87234 39,-9.49984 ' +
-            'l 0,-' + data + ' ' +
-            'c -0.0729,-7.926124 -30.54171,-9.269804 -39,-9.499835 z';
+        function composePath(data) {
+            var topLeftX = 0,
+                topLeftY = 0,
+                topRightX = 78,
+                topRightY = 0,
+                bezierPoint1X = 10,
+                bezierPoint1Y = 15,
+                bezierPoint2X = 76,
+                bezierPoint2Y = 11;
+
+            return 'm 281,73 0,558.436651 ' +
+                'c 10,15 ' +
+                '76,11 ' +
+                '78,0 '+
+                'l '+topLeftX+',-' + (topLeftY+data) + ' ' +
+                'c -'+bezierPoint1X+',-'+bezierPoint1Y+' ' +
+                '-'+bezierPoint2X+',-'+bezierPoint2Y+' ' +
+                '-'+topRightX+','+topRightY+' z'
         }
     }
 
