7125ee46039c44b9864b301227017db1c1b9ecdd
yave <yannick.vergeylen@gmail.com>
2016-04-20 14:49:06 +0200

    slower animation, listening to socket, no automatic animation now

--- www/booze/socket.service.js
+++ www/booze/socket.service.js
@@ -5,9 +5,9 @@
         .module('booze')
         .service("socketService", socketService);
 
-    socketService.$inject = ['$interval'];
+    socketService.$inject = ['$interval','$window'];
 
-    function socketService($interval) {
+    function socketService($interval, $window) {
 
         //Internal variables.
         var nodeSocket = null;
@@ -25,31 +25,42 @@
         };
 
         function connectToWebsocket() {
-            //nodeSocket = new WebSocket("ws://localhost:8081");
-            //
-            ////Wait for the socket connection to be established before doing anything else socket related!
-            //nodeSocket.onopen = function (event) {
-            //    console.log("Connection to web socket established!");
-            //
-            //    nodeSocket.onmessage = function (event) {
-            //        console.log("Received message from web socket:" + event.data);
-            //        var data = JSON.parse(event.data);
-            //
-            //        for(var i = 0; i < callbacks.length; i++) {
-            //            callbacks[i](data);
-            //        }
-            //    }
-            //};
+            nodeSocket = new WebSocket("ws://"+$window.location.hostname+":7081");
 
-            var height = 100;
+            //Wait for the socket connection to be established before doing anything else socket related!
+            nodeSocket.onopen = function (event) {
+                console.log("Connection to web socket established!");
 
-            $interval(function(){
-                height!==0? height -= 25 : height = 100;
+                nodeSocket.onmessage = function (event) {
+                    console.log("Received message from web socket:" + event.data);
+                    var data = JSON.parse(event.data);
 
-                callbacks.forEach(function(cb){
-                    cb(height)
-                });
-            },2000);
+                    for(var i = 0; i < callbacks.length; i++) {
+                        switch(data.level){
+                            case 'HIGH' :
+                                callbacks[i](75);
+                            break;
+                            case 'MEDIUM' :
+                                callbacks[i](50);
+                            break;
+                            case 'LOW' :
+                                callbacks[i](25);
+                                break;
+                            default :
+                        }
+                    }
+                }
+            };
+
+            //var height = 100;
+            //
+            //$interval(function(){
+            //    height!==0? height -= 25 : height = 100;
+            //
+            //    callbacks.forEach(function(cb){
+            //        cb(height)
+            //    });
+            //},10000);
         }
     }
 
