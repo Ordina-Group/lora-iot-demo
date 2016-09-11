8dafb66250d483ddf9f4713b20bc225856a5a606
Kevin Van den Abeele <info@beeles-place.be>
2016-09-11 16:16:35 +0200

    Arduino code tested and working (interval interrupt is still glitchy)

--- device-code-and-schemas/jax-arduino-code/Jax/Jax.ino
+++ device-code-and-schemas/jax-arduino-code/Jax/Jax.ino
@@ -1,19 +1,8 @@
 #include <Arduino.h>
 #include "DHT.h"
 
-#define DHTPIN 2     // what pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
-// to 3.3V instead of 5V!
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+#define DHTPIN 8
+#define DHTTYPE DHT22
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -31,7 +20,7 @@ void setup() {
   pinMode(interruptPinInterval, INPUT_PULLUP);
   
   attachInterrupt(digitalPinToInterrupt(interruptPinSingle), singleButtonPressed, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(detectInterval), intervalButtonPressed, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(interruptPinInterval), intervalButtonPressed, CHANGE);
 }
 
 void loop() {
